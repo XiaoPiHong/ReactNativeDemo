7b5662b591dd947eb86c1baac6db5d629175c8bc
zhouzhenhong <xiaopihong0325@qq.com>
2024-11-12 17:00:44 +0800

    fix: 初步实现xupdate

--- App.tsx
+++ App.tsx
@@ -9,11 +9,78 @@ import ThemeProvider from "@/context/useThemeContext";
 import {NoInternetToast} from "@/components/NoInternet";
 import SplashScreen from "react-native-splash-screen";
 import RootNavigation from "@/components/RootNavigation";
+import {XUpdate, UpdateArgs, InitArgs} from "@/utils/xupdate";
 
 function App(): React.JSX.Element {
+  //自定义的异常处理
+  const errorListener = error => {
+    //下载失败
+    if (error.code === 4000) {
+      XUpdate.showRetryUpdateTip(
+        "Github被墙无法继续下载，是否考虑切换蒲公英下载？",
+        "https://www.pgyer.com/flutter_learn",
+      );
+    }
+    console.log({
+      _message: "发送异常：" + JSON.stringify(error),
+    });
+  };
+
+  //自定义解析
+  const customParser = json => {
+    let appInfo = JSON.parse(json);
+    return {
+      //必填
+      hasUpdate: appInfo.hasUpdate,
+      versionCode: appInfo.versionCode,
+      versionName: appInfo.versionName,
+      updateContent: appInfo.updateLog,
+      downloadUrl: appInfo.apkUrl,
+      //选填
+      isIgnorable: appInfo.isIgnorable,
+      apkSize: appInfo.apkSize,
+    };
+  };
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
+
+  const checkUpdateDefault = () => {
+    let args = new UpdateArgs(
+      "https://gitee.com/xuexiangjys/XUpdate/raw/master/jsonapi/update_test.json",
+    );
+
+    console.log(args);
+    // args.themeColor = '#FF14388E';
+    // args.buttonTextColor = '#FFFFFFFF';
+    XUpdate.update(args);
+  };
+
+  useEffect(() => {
+    let args = new InitArgs();
+    args.debug = true;
+    args.isPostJson = false;
+    args.timeout = 25000;
+    args.isWifiOnly = false;
+    args.isAutoMode = false;
+    args.supportSilentInstall = false;
+    args.enableRetry = false;
+
+    XUpdate.init(args)
+      .then(result => {
+        console.log("初始化成功:" + JSON.stringify(result));
+
+        checkUpdateDefault();
+      })
+      .catch(error => {});
+
+    // 设置自定义解析
+    XUpdate.setCustomParser({parseJson: customParser});
+    // 设置错误监听
+    XUpdate.addErrorListener(errorListener);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <ReduxProvider>
