9ae2d03e6dc33117e5822b2c240b5feb9116aea5
zhouzhenhong <xiaopihong0325@qq.com>
2024-11-13 17:58:58 +0800

    fix: 新增api

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,8 +1,8 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
-  username: "xiaopihong",
-  token: "xxx",
+  username: "",
+  token: "",
 };
 
 const userSlice = createSlice({
